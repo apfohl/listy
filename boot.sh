e81ee084f2d79815b944d46820af9e5a9f456750
Andreas Pfohl <mail@apfohl.com>
2019-07-30 18:08:30 +0200

    Solved some restart issues

--- boot.sh
+++ boot.sh
@@ -15,10 +15,16 @@ fi
 # Fix Mailman file permissions
 /usr/local/mailman/bin/check_perms -f
 
-# Remove stale FCGI Wrap socket
+# Remove stale unix sockets, pid files and locks
 if [ -S "/var/run/fcgiwrap.sock" ]; then
     rm -f /var/run/fcgiwrap.sock
 fi
+
+if [ -f "/var/run/rsyslogd.pid" ]; then
+    rm -f /var/run/rsyslogd.pid
+fi
+
+rm -f /data/mailman/locks/*
 
 # Start services
 /usr/bin/supervisord -c /etc/supervisord.conf
