78b4d8179d09874f0b53a3912aff0ce2a742b84b
Andreas Pfohl <mail@apfohl.com>
2019-09-23 23:16:28 +0200

    Implemented useful templates for panel application

--- panel/panel.c
+++ panel/panel.c
@@ -12,7 +12,8 @@ const int CHUNK = 1024;
 
 extern char **environ;
 
-char *read_file(const char *path) {
+char *read_file(const char *path)
+{
     char *result = NULL;
     size_t result_size = 0;
 
@@ -21,7 +22,8 @@ char *read_file(const char *path) {
     char buffer[CHUNK];
     size_t nread = 0;
 
-    while ((nread = fread(buffer, sizeof(char), CHUNK, file)) > 0) {
+    while ((nread = fread(buffer, sizeof(char), CHUNK, file)) > 0)
+    {
         result = realloc(result, (result_size + nread) * sizeof(char));
         memcpy(result + result_size, buffer, nread);
         result_size += nread;
@@ -36,46 +38,18 @@ char *read_file(const char *path) {
     return result;
 }
 
-/*static void PrintEnv(FCGX_Stream *out, char *label, char **envp)
+static void PrintEnv(FCGX_Stream *out, char *label, char **envp)
 {
     FCGX_FPrintF(out, "<p>%s:</p>\n<pre>\n", label);
-    for(; *envp != NULL; envp++) {
+    for (; *envp != NULL; envp++)
+    {
         FCGX_FPrintF(out, "%s\n", *envp);
     }
     FCGX_FPrintF(out, "</pre>\n");
-}*/
-
-static void print_row(FCGX_Stream *out, struct jzon *jzon) {
-    const char *queue_id = jzon_object_get(jzon, "queue_id", NULL)->string;
-    const char *queue_name = jzon_object_get(jzon, "queue_name", NULL)->string;
-    const int message_size =
-        (int) jzon_object_get(jzon, "message_size", NULL)->number;
-    const int arrival_time =
-        (int) jzon_object_get(jzon, "arrival_time", NULL)->number;
-    const char *sender = jzon_object_get(jzon, "sender", NULL)->string;
-    const struct jzon_array *recipients =
-        jzon_object_get(jzon, "recipients", NULL)->array;
-
-    FCGX_FPrintF(out,
-        "<tr><td>%s</td><td>%s</td><td>%d</td><td>%d</td><td>%s</td></tr>\n",
-        queue_id, queue_name, message_size, arrival_time, sender);
-
-    FCGX_FPrintF(out, "<tr><td colspan=\"5\"><ul>");
-
-    for (int i = 0; i < recipients->capacity; i++) {
-        struct jzon *recipient = recipients->elements[i];
-
-        FCGX_FPrintF(out, "<li><strong>%s</strong> (%s)</li>",
-            jzon_object_get(recipient, "address", NULL)->string,
-            jzon_object_get(recipient, "delay_reason", NULL)->string);
-    }
-
-    FCGX_FPrintF(out, "</ul></td></tr>\n");
 }
 
-static size_t render_template(FCGX_Stream *out, const char *path,
-    TMPL_varlist *varlist) {
-
+static char *render_template(const char *path, TMPL_varlist *varlist)
+{
     char *template = NULL;
     size_t template_size = 0;
 
@@ -86,78 +60,106 @@ static size_t render_template(FCGX_Stream *out, const char *path,
     fflush(template_stream);
     fclose(template_stream);
 
-    FCGX_FPrintF(out, template);
+    return template;
+}
 
-    free(template);
+static char *render_layout(const char *path,
+                           const char *title,
+                           const char *content)
+{
 
-    return template_size;
+    TMPL_varlist *varlist = TMPL_add_var(NULL, "title", title, "content",
+                                         content, 0);
+    char *template = render_template(path, varlist);
+    TMPL_free_varlist(varlist);
+
+    return template;
+}
+
+static char *render_postqueue(struct jzon *jzon)
+{
+    TMPL_varlist *varlist = NULL;
+    TMPL_varlist *mainlist = NULL;
+    TMPL_loop *queue_entries = NULL;
+    TMPL_loop *recipient_entries = NULL;
+
+    const struct jzon_array *recipients =
+        jzon_object_get(jzon, "recipients", NULL)->array;
+
+    char size[16];
+    snprintf(size, 16, "%d", (int)jzon_object_get(jzon, "message_size", NULL)->number);
+
+    char arrival_time[16];
+    snprintf(arrival_time, 16, "%d", (int)jzon_object_get(jzon, "arrival_time", NULL)->number);
+
+    // add information
+    varlist = TMPL_add_var(
+        varlist,
+        "queue_id", jzon_object_get(jzon, "queue_id", NULL)->string,
+        "status", jzon_object_get(jzon, "queue_name", NULL)->string,
+        "size", size,
+        "arrival_time", arrival_time,
+        "sender", jzon_object_get(jzon, "sender", NULL)->string,
+        0);
+
+    // add recipients
+    for (int i = 0; i < recipients->capacity; i++)
+    {
+        struct jzon *recipient = recipients->elements[i];
+
+        TMPL_varlist *vl_recipient = TMPL_add_var(
+            NULL,
+            "address", jzon_object_get(recipient, "address", NULL)->string,
+            "delay_reason", jzon_object_get(recipient, "delay_reason", NULL)->string,
+            0);
+
+        recipient_entries = TMPL_add_varlist(recipient_entries, vl_recipient);
+    }
+
+    // Add recipients
+    varlist = TMPL_add_loop(varlist, "recipient_entries", recipient_entries);
+
+    // add entry to queue
+    queue_entries = TMPL_add_varlist(queue_entries, varlist);
+
+    // add queue to main list
+    mainlist = TMPL_add_loop(mainlist, "queue_entries", queue_entries);
+
+    // render template
+    char *postqueue = render_template("templates/postqueue.tmpl", mainlist);
+
+    // free varlist
+    TMPL_free_varlist(mainlist);
+
+    return postqueue;
 }
 
 int main(int argc, char **argv)
 {
     FCGX_Stream *in, *out, *err;
     FCGX_ParamArray envp;
-    int count = 0;
 
     const char *title = "Postfix Mail Queue";
 
-    while(FCGX_Accept(&in, &out, &err, &envp) >= 0) {
+    while (FCGX_Accept(&in, &out, &err, &envp) >= 0)
+    {
         char *json = read_file("queue.json");
         struct jzon *jzon = jzon_parse(json, NULL);
         free(json);
 
-        // char *content_length = FCGX_GetParam("CONTENT_LENGTH", envp);
-        // int len = 0;
-
         FCGX_FPrintF(out, "Content-type: text/html\nStatus: 200\r\n\r\n");
 
-        TMPL_varlist *varlist = TMPL_add_var(0, "title", title, 0);
-        (void) render_template(out, "layout.tmpl", varlist);
-        TMPL_free_varlist(varlist);
+        char *postqueue = render_postqueue(jzon);
 
-        FCGX_FPrintF(out, "<p>Request number %d,  Process ID: %d</p>\n",
-           ++count, getpid());
+        char *layout =
+            render_layout("templates/layout.tmpl", title, postqueue);
+        FCGX_FPrintF(out, layout);
 
-        FCGX_FPrintF(out,
-            "<p>\n"
-            "<table class=\"table table-sm\">\n"
-            "<thead><tr><th scope=\"col\">Queue ID</th><th scope=\"col\">Status</th><th scope=\"col\">Size</th><th scope=\"col\">Arrival Time</th><th scope=\"col\">Sender</th></tr></thead>\n"
-            "<tbody>\n");
-
-        print_row(out, jzon);
-
-        FCGX_FPrintF(out,
-            "</tbody>\n"
-            "</table>\n"
-            "</p>\n");
-
-        /*if (content_length != NULL)
-            len = strtol(content_length, NULL, 10);
-
-        if (len <= 0) {
-            FCGX_FPrintF(out, "<p>No data from standard input.</p>\n");
-        } else {
-            int i, ch;
-
-            FCGX_FPrintF(out, "<p>Standard input:</p>\n<pre>\n");
-            for (i = 0; i < len; i++) {
-                if ((ch = FCGX_GetChar(in)) < 0) {
-                    FCGX_FPrintF(out,
-                        "Error: Not enough bytes received on standard input.\n");
-                    break;
-                }
-                FCGX_PutChar(ch, out);
-            }
-            FCGX_FPrintF(out, "\n</pre>\n");
-        }
+        free(layout);
+        free(postqueue);
 
         PrintEnv(out, "Request environment", envp);
-        PrintEnv(out, "Initial environment", environ);*/
-
-        FCGX_FPrintF(out,
-            "</div>\n"
-            "</body>\n"
-            "</html>\n");
+        PrintEnv(out, "Initial environment", environ);
 
         jzon_free(jzon);
     }
